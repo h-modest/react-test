2482da921517c1246b965e78f691d741ddd72fa6
hxq5651253 <hxq@yumeng.co>
2016-10-18 09:20:04 +0800

    next

--- src/components/Index/IndexHead.js
+++ src/components/Index/IndexHead.js
@@ -4,6 +4,17 @@ class IndexHead extends React.Component {
   render() {
     return (
       <div className="admin-head">
+        <ul className="admin-head-right">
+          <li>
+            <i className="fa fa-bell-o"></i>通知
+          </li>
+          <li>
+            <i className="fa fa-question"></i>帮助
+          </li>
+          <li>
+            <i className="fa fa-question"></i>
+          </li>
+        </ul>
       </div>
     );
   }
